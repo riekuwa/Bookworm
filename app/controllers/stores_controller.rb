18d43b42587d27ff5fa98b98ec39c77b0ae85a6e
riekuwa <riekuwa1126@gmail.com>
2016-05-06 17:45:32 +0900

    書店情報の投稿フォーム作成

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,4 +3,17 @@ class StoresController < ApplicationController
     @stores = Store.all
   end
 
+  def new
+  end
+
+  def create
+    Store.create(store_params)
+  end
+
+  private
+  def store_params
+    params.permit(:name, :station, :distance, :phone_number, :open, :close)
+  end
+
+
 end
